69f16ea449abbf8920b809d6fe2e0eb65bb9ed66
David <david25042001@gmail.com>
2024-03-10 13:45:15 +0100

    ultimo commit

--- apiserver/src/main/java/com/xyz/openapi/server/config/SwaggerConfig.java
+++ apiserver/src/main/java/com/xyz/openapi/server/config/SwaggerConfig.java
@@ -5,10 +5,26 @@ import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfiguration {
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().info(new Info().title("OpenAPI Sample App").version("1.0"));
+        Contact contact = new Contact();
+        contact.setEmail("help@xyz.com");
+        contact.setName("XYZ Support");
+        contact.setUrl("http://www.xyz.com");
+        return new OpenAPI()
+                .info(new Info().title("Employee APIs").description("Description here..")
+                        .version("1.0").contact(contact)
+                        .license(new License().name("2015-2021 XYZ LLC All Rights Reserved")));
     }
 }
+
+// @Configuration
+// public class SwaggerConfig {
+//     @Bean
+//     public OpenAPI openAPI() {
+//         return new OpenAPI().info(new Info().title("OpenAPI Sample App").version("1.0"));
+//     }
+// }
